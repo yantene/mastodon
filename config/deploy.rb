4796af08fcfc1ff346c6cd4ad16bd9513b0abddd
yantene <contact@yantene.net>
2019-02-05 01:23:40 +0900

    edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,12 @@
 
 lock '3.11.0'
 
-set :repo_url, ENV.fetch('REPO', 'https://github.com/tootsuite/mastodon.git')
-set :branch, ENV.fetch('BRANCH', 'master')
+set :repo_url, ENV.fetch('REPO', 'https://github.com/fla-red/mastodon.git')
+set :branch, ENV.fetch('VERSION', 'fla.red')
 
 set :application, 'mastodon'
-set :rbenv_type, :user
 set :rbenv_ruby, File.read('.ruby-version').strip
 set :migration_role, :app
 
-append :linked_files, '.env.production', 'public/robots.txt'
+#append :linked_files, 'public/robots.txt'
 append :linked_dirs, 'vendor/bundle', 'node_modules', 'public/system'
